abb79714d78b30d814d0216c7f59b902a9ada639
Kiran kumar <kirankirankumar56@gmail.com>
2026-07-27 17:48:55 +0530

    Add: dynamicArray - submission-5

---
+++ Data Structures & Algorithms/dynamicArray/submission-5.js
@@ -0,0 +1,75 @@
+class DynamicArray {
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.array = new Array(capacity);
+        this.size = 0;
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i) {
+        return this.array[i];
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i, n) {
+        this.array[i] = n;
+        // this.size++;
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n) {
+        if(this.size === this.capacity) {
+            this.resize();
+        }
+        // const newArr = this.array.slice(n, 1);
+        this.array[this.size] = n;
+        this.size++;
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback() {
+        const temp = this.array[this.size-1];
+        this.array[this.size-1] = null;
+        this.size--;
+        return temp;
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize() {
+        const newArr = new Array(this.capacity);
+        this.array = [...this.array, ...newArr];
+        this.capacity = this.capacity ? this.capacity * 2 : 1
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize() {
+        return this.size;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity() {
+        return this.capacity;
+    }
+}
