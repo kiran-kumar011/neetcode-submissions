4e31fcd98fbc31e24c6681237a3aae1d8e3113d7
Kiran kumar <kirankirankumar56@gmail.com>
2026-07-30 14:43:26 +0530

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // for(let i=0; i < nums.length; i++) {
+        //     for(let j=i+1; j < nums.length; j++) {
+        //         if(nums[i] + nums[j] === target) {
+        //             return [i, j]
+        //         }
+        //     }
+        // }
+        const map = {};
+        for(let i=0; i<nums.length; i++) {
+            const diff = target - nums[i];
+            console.log(diff,"diff")
+            if(diff in map) {
+                return [map[diff], i]
+            } 
+            map[nums[i]] = i
+        }        
+    }
+
+}
